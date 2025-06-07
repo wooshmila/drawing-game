8568429b06d6c66b78e9bfa53ae544fec9353503
MichalKot386 <c3022545@newcastle.ac.uk>
2025-06-07 14:39:23 +0100

    Create server

--- Pages/app.py
+++ Pages/app.py
@@ -3,5 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p></p>"
+def server():
+    return "<p>Hello</p>"
+
+if __name__ == '__main__':
+    app.run(debug=True)

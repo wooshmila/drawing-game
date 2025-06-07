a3d718739c463235f3f13f58ded71a395426154e
MichalKot386 <c3022545@newcastle.ac.uk>
2025-06-07 15:40:28 +0100

    added routes to signup and login

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/signup", methods=['GET','POST'])
+def signup():
+    return render_template("signup.html")
+
+@app.route("/login", methods=['GET','POST'])
+def login():
+    return render_template("login.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
